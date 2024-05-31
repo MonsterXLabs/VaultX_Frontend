2d09b3a675e258fa65332a7773bf2f0dba765dd8
Akhil Bharti <akhil.bh96@gmail.com>
2024-05-31 14:17:37 +0530

    added price conversion

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,31 @@
-export const chain = process.env.REACT_APP_NODE_ENV === "DEV" ? "80001" : "137"
+export const chain = process.env.REACT_APP_NODE_ENV === "DEV" ? "80002" : "137";
 
-export const contractAddress = process.env.REACT_APP_NODE_ENV === "DEV" ? "0xAAdcdEC98CE6C560C6e4b1C2B1b31258D5C1AF9A" : "0x871679454DFEA476ca5fa96920b6BA2a1e08876b"
+export const contractAddress =
+  process.env.REACT_APP_NODE_ENV === "DEV"
+    ? "0xAAdcdEC98CE6C560C6e4b1C2B1b31258D5C1AF9A"
+    : "0xCd5d3534544D8A7856e41CAfFca535113c61A4EB";
 
-export const explorer = process.env.REACT_APP_NODE_ENV === "DEV" ? "https://mumbai.polygonscan.com/" : "https://polygonscan.com"
+export const explorer =
+  process.env.REACT_APP_NODE_ENV === "DEV"
+    ? "https://amoy.polygonscan.com/"
+    : "https://polygonscan.com";
 
-export const network = process.env.REACT_APP_NODE_ENV === "DEV" ? "Mumbai" : "Polygon"
+export const network =
+  process.env.REACT_APP_NODE_ENV === "DEV" ? "Amoy" : "Polygon";
+
+// const DEV_CONFIG = {
+//   chain: "80002",
+//   contractAddress: "0xAAdcdEC98CE6C560C6e4b1C2B1b31258D5C1AF9A",
+//   explorer: "https://amoy.polygonscan.com/",
+//   network: "Amoy",
+// };
+
+// const PROD_CONFIG = {
+//   chain: "137",
+//   contractAddress: "0xCd5d3534544D8A7856e41CAfFca535113c61A4EB",
+//   explorer: "https://polygonscan.com",
+//   network: "Polygon",
+// };
+
+// export const config =
+//   process.env.REACT_APP_NODE_ENV === "DEV" ? DEV_CONFIG : PROD_CONFIG;
