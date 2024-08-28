d90444f294d694303b4afc2ed356c9abebd5198f
plvannemanja <plvannemanja@gmail.com>
2024-08-28 03:21:55 -0700

    chore: update appreciation

--- src/components/Dashboard/NFTCards/All.js
+++ src/components/Dashboard/NFTCards/All.js
@@ -3,25 +3,25 @@ import { NftServices } from "../../../services/supplier"
 import { Link } from "react-router-dom"
 import useDebounce from "../../../customHook/useDebounce"
 
-function All({tab, category, searchInput, filter}) {
+function All({ tab, category, searchInput, filter }) {
   const [nfts, setNfts] = useState([])
 
   const nftService = new NftServices()
   async function getUserNfts() {
-    try{
-      if(tab !== "Profile"){
-        const dat = await nftService.getAllNfts({skip: 0, limit: 0, category: category?.name, searchInput: searchInput? searchInput: "", filter: filter.filter})
+    try {
+      if (tab !== "Profile") {
+        const dat = await nftService.getAllNfts({ skip: 0, limit: 0, category: category?.name, searchInput: searchInput ? searchInput : "", filter: filter.filter })
         setNfts(dat.data.success ? dat.data.nfts[0].data : [])
-      }else{
-        const {data:{nfts}} = await nftService.getNftOfUser({searchInput: searchInput? searchInput: "", filter: filter.filter})
+      } else {
+        const { data: { nfts } } = await nftService.getNftOfUser({ searchInput: searchInput ? searchInput : "", filter: filter.filter })
         setNfts(nfts ? nfts : [])
       }
-    }catch(e){
+    } catch (e) {
       setNfts([])
     }
   }
 
-  const debounceSearch = useDebounce(getUserNfts,1000)
+  const debounceSearch = useDebounce(getUserNfts, 1000)
   useEffect(() => {
     debounceSearch()
   }, [category, searchInput, filter])
@@ -29,7 +29,7 @@ function All({tab, category, searchInput, filter}) {
   return (
     <div className="categorie__card__blk">
       <div className="row g-4">
-        {nfts?.length> 0 && nfts.filter((nft)=>(!nft?.active && !nft.ownerInfo?.[0]?.active && !nft.curationInfo?.[0].active)).map((nft,index) => (
+        {nfts?.length > 0 && nfts.filter((nft) => (!nft?.active && !nft.curationInfo?.[0].active)).map((nft, index) => (
           <Link
             to={`/dashboard/nft/${nft._id}`}
             className="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6"
@@ -37,7 +37,7 @@ function All({tab, category, searchInput, filter}) {
           >
             <div className="single__sport__blk">
               <div className="sport__thumb">
-                <img className="w-full !aspect-[4/3] !object-cover" src={nft.cloudinaryUrl} alt=""/>
+                <img className="w-full !aspect-[4/3] !object-cover" src={nft.cloudinaryUrl} alt="" />
               </div>
               <div className="sport__content">
                 <h5>{nft.name}</h5>
